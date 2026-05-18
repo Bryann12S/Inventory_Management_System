4b5d9a15347f94d6f3e52a48204dddbcda8e66fc
Bryann12S <bryanshiguango6@gmail.com>
2026-05-17 21:20:04 -0500

    feat(backend): automate entity framework database migrations on application startup

--- backend/Program.cs
+++ backend/Program.cs
@@ -112,4 +112,20 @@ app.MapStockMovementEndpoints();
 //RegisterRoutes of Auth
 app.MapAuthEndpoints();
 
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    try
+    {
+        var context = services.GetRequiredService<AppDbContext>();
+        
+        await context.Database.MigrateAsync();
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "Ocurrió un error al aplicar las migraciones automáticas.");
+    }
+}
+
 app.Run();
